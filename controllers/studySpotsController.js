598ceb4da4311949a0cdaf99095315039ec0efbd
lancechincodes <lanceechin@gmail.com>
2022-08-07 16:29:16 -0500

    Edit routes for study spot controllers

--- controllers/studySpotsController.js
+++ controllers/studySpotsController.js
@@ -14,5 +14,41 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+// GET one study spot
+router.get('/:studySpotId', async (req, res, next) => {
+    try {
+        const studySpot = await StudySpot.findById(req.params.studySpotId)
+        studySpot ? res.status(200).json(studySpot) : res.sendStatus(404)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+// POST create/add a study spot from google maps API when a review is written
+// If study spot already has a review, send existing study spot (GET)
+// otherwise, create new study spot (POST)
+router.post('/', async (req, res, next) => {
+    try {
+        const newStudySpot = await StudySpot.create(req.body)
+        res.status(201).json(newStudySpot)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+// DELETE a study spot
+// If all reviews are deleted for a study spot, it may deleted from the database
+router.delete('/:studySpotId', async (req, res, next) => {
+    try {
+        const deletedStudySpot = await StudySpot.findByIdAndDelete(req.params.studySpotId)
+        deletedStudySpot ? res.sendStatus(204) : res.sendStatus(404)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
 // export the routes
 module.exports = router
